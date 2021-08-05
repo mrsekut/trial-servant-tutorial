b415166bc43edb911409a42fd29f6f8f7fbd26c6
mrsekut <k.cloudspider@gmail.com>
2021-08-05 22:06:22 +0900

    feat: first endpoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import qualified Server as S
 
 main :: IO ()
-main = startApp
+main = S.main
---
+++ src/Server.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
+
+module Server where
+
+import           Prelude                       ()
+import           Prelude.Compat
+
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Data.Aeson
+import qualified Data.Aeson.Parser
+import           Data.Aeson.Types
+import           Data.Attoparsec.ByteString
+import           Data.ByteString               (ByteString)
+import           Data.List
+import           Data.Maybe
+import           Data.String.Conversions
+import           Data.Time.Calendar
+import           GHC.Generics
+import           Lucid
+import           Network.HTTP.Media            ((//), (/:))
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+-- import           Servant.Types.SourceT
+import           System.Directory
+import           Text.Blaze
+import qualified Text.Blaze.Html
+import           Text.Blaze.Html.Renderer.Utf8
+
+
+type UserAPI = "users" :> Get '[JSON] [User]
+
+data User = User
+  { name              :: String
+  , age               :: Int
+  , email             :: String
+  , registration_date :: Day
+  } deriving (Show, Eq, Generic)
+
+
+instance ToJSON User
+
+
+users :: [User]
+users =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
+  , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+  ]
+
+server :: Server UserAPI
+server = pure users
+
+userAPI :: Proxy UserAPI
+userAPI = Proxy
+
+app :: Application
+app = serve userAPI server
+
+main :: IO ()
+main = run 8081 app
